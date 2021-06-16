d511232acb3a0408fd163854763b91bc776c27af
Evgen Gulich <finomalinotv@gmail.com>
2021-06-16 21:37:56 +0300

    add price counter

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,18 @@
           :disabled="this.type === ''"
           v-model="marks"
           label="name"
+          resetOnOptionsChange
+          @input="getValueSecond"
+        ></v-select>
+      </div>
+      <div class="w-1/2 lg:w-1/3 p-1">
+        <h3>Модели</h3>
+        <v-select
+          :options="this.$store.state.options.OPTIONS_MODEL"
+          :disabled="this.type === '' || this.marks === ''"
+          v-model="model"
+          label="name"
+          resetOnOptionsChange
         ></v-select>
       </div>
       <div class="w-1/2 lg:w-1/3 p-1">
@@ -31,7 +43,49 @@
           :disabled="this.type === ''"
           v-model="bodystyles"
           label="name"
+          resetOnOptionsChange="true"
         ></v-select>
+      </div>
+      <div class="w-1/2 lg:w-1/3 p-1">
+        <h3>Коробка передач</h3>
+        <v-select
+          :options="this.$store.state.options.OPTIONS_GEARBOXES"
+          :disabled="this.type === ''"
+          v-model="gearboxes"
+          label="name"
+          resetOnOptionsChange="true"
+        ></v-select>
+      </div>
+      <div class="w-1/2 lg:w-1/3 p-1">
+        <h3>Типы привода</h3>
+        <v-select
+          :options="this.$store.state.options.OPTIONS_DRIVERTYPES"
+          :disabled="this.type === ''"
+          v-model="driverTypes"
+          label="name"
+          resetOnOptionsChange="true"
+        ></v-select>
+      </div>
+    </div>
+    <div class="flex flex-row px-20 py-8 justify-between">
+      <button
+        class="
+          px-12
+          py-5
+          text-3xl text-blue-900
+          bg-gray-50
+          hover:bg-blue-800
+          border-2
+          rounded-sm
+          border-blue-800
+          hover:text-white
+        "
+        @click="getPrice"
+      >
+        GO
+      </button>
+      <div class="text-4xl text-indigo-700 text-justify flex items-center">
+        <p><span class="text-black">Цена:</span> {{ price }}</p>
       </div>
     </div>
   </div>
@@ -49,12 +103,23 @@ export default {
       type: "",
       marks: "",
       bodystyles: "",
+      gearboxes: "",
+      driverTypes: "",
+      model: "",
     };
   },
   mounted() {
     this.getOptionsType();
   },
-  computed: {},
+  computed: {
+    price() {
+      if (isNaN(this.$store.state.options.PRICE.arithmeticMean)) {
+        return "price no";
+      } else {
+        return Math.round(this.$store.state.options.PRICE.arithmeticMean);
+      }
+    },
+  },
   methods: {
     getOptionsType() {
       this.$store.dispatch("GET_OPTIONS", {
@@ -72,9 +137,32 @@ export default {
         stateName: "SET_OPTIONS_BODYSTYLES",
         url: `categories/${this.type.value}/bodystyles?`,
       });
+      this.$store.dispatch("GET_OPTIONS", {
+        stateName: "SET_OPTIONS_GEARBOXES",
+        url: `categories/${this.type.value}/gearboxes?`,
+      });
+      this.$store.dispatch("GET_OPTIONS", {
+        stateName: "SET_OPTIONS_DRIVERTYPES",
+        url: `categories/${this.type.value}/driverTypes?`,
+      });
     },
-    test() {
-      console.log(this.marks);
+    getValueSecond() {
+      this.$store.dispatch("GET_OPTIONS", {
+        stateName: "SET_OPTIONS_MODEL",
+        url: `categories/${this.type.value}/marks/${this.marks.value}/models?`,
+      });
+    },
+    getPrice() {
+      let url = "";
+      if (this.marks) {
+        url = url + `&marka_id=${this.marks.value}`;
+      }
+      if (this.model) {
+        url = url + `&model_id${this.model.value}`;
+      }
+      this.$store.dispatch("GET_PRICE", {
+        url: url,
+      });
     },
   },
 };
--- src/store/option.js
+++ src/store/option.js
@@ -3,9 +3,13 @@ import { API_BASE_URl, API_KEY } from "../utils/constant";
 
 export default {
   state: {
+    PRICE: "0",
     OPTIONS_TYPE: [],
     OPTIONS_MARKS: [],
+    OPTIONS_MODEL: [],
     OPTIONS_BODYSTYLES: [],
+    OPTIONS_GEARBOXES: [],
+    OPTIONS_DRIVERTYPES: [],
     VALUE_TYPE: "",
     VALUE_BODYSTYLES: "",
   },
@@ -16,11 +20,23 @@ export default {
     SET_OPTIONS_MARKS(state, payload) {
       state.OPTIONS_MARKS = payload;
     },
+    SET_OPTIONS_MODEL(state, payload) {
+      state.OPTIONS_MODEL = payload;
+    },
     SET_OPTIONS_BODYSTYLES(state, payload) {
       state.OPTIONS_BODYSTYLES = payload;
     },
+    SET_OPTIONS_GEARBOXES(state, payload) {
+      state.OPTIONS_GEARBOXES = payload;
+    },
+    SET_OPTIONS_DRIVERTYPES(state, payload) {
+      state.OPTIONS_DRIVERTYPES = payload;
+    },
     SET_VALUE_TYPE(state, payload) {
       state.VALUE_TYPE = payload;
+    },
+    SET_PRICE(state, payload) {
+      state.PRICE = payload;
     },
   },
   actions: {
@@ -30,6 +46,17 @@ export default {
         .then((data) => data.data)
         .then((data) => {
           commit(stateName, data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    async GET_PRICE({ commit }, { url }) {
+      await axios
+        .get(`${API_BASE_URl}/average_price?${API_KEY}${url}`)
+        .then((data) => data.data)
+        .then((data) => {
+          commit("SET_PRICE", data);
         })
         .catch((err) => {
           console.log(err);
